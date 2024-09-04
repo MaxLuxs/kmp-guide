4a7c0d207ca1879f7dc92a19e86e628947c74274
maxluxs <max.developer.luxs@gmail.com>
2024-09-05 01:32:12 +0300

    Added immutable to ui state

--- composeApp/src/commonMain/kotlin/io/maxluxs/kmp_guide/common/UiState.kt
+++ composeApp/src/commonMain/kotlin/io/maxluxs/kmp_guide/common/UiState.kt
@@ -1,5 +1,8 @@
 package io.maxluxs.kmp_guide.common
 
+import androidx.compose.runtime.Immutable
+
+@Immutable
 sealed interface UiState<T> {
     class Loading<T>: UiState<T>
     data class Success<T>(val data: T): UiState<T>

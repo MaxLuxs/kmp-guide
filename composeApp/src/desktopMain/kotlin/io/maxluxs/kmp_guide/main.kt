97091186811d44e042031f68f072c7b601a81634
maxluxs <max.developer.luxs@gmail.com>
2024-09-06 00:56:25 +0300

    Added window size observer to desktop

--- composeApp/src/desktopMain/kotlin/io/maxluxs/kmp_guide/main.kt
+++ composeApp/src/desktopMain/kotlin/io/maxluxs/kmp_guide/main.kt
@@ -1,14 +1,20 @@
 package io.maxluxs.kmp_guide
 
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
+import io.maxluxs.kmp_guide.common.WindowSize
 import io.maxluxs.kmp_guide.features.main.App
 
 fun main() = application {
+    val windowState = rememberWindowState(size = DpSize(1280.dp, 720.dp))
     Window(
+        state = windowState,
         onCloseRequest = ::exitApplication,
         title = "KMP_Guide",
     ) {
-        App()
+        App(WindowSize.basedOnWidth(windowState.size.width))
     }
 }
